9e9724b4d9b8f89bde9b140118be3e9005bc9b17
Cameron O'Brien <piggiescanfly238@gmail.com>
2016-12-18 01:01:56 -0800

    Redid the file structure slightly, added some new stuff dir-wise.

---
+++ app/views/__init__.py
@@ -0,0 +1,7 @@
+from . import (
+    index
+)
+
+_page_locations = [
+    'Home'
+]
--- app/views/index.py
+++ app/views/index.py
@@ -1,8 +1,12 @@
 from flask import render_template
+
 from app import app
 
 
 @app.route('/index')
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(
+        'index/index.html',
+        page_location="Home"
+    )
